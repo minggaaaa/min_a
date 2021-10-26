e905dc0468a739da813be78fe9e6508518cad786
minggaaaa <92001539+minggaaaa@users.noreply.github.com>
2021-10-26 09:17:39 +0900

    python20211015

---
+++ 20211015/b.py
@@ -0,0 +1,8 @@
+from a import greet,greet2
+
+greet()
+greet2('john',23)
+greet2('john',"23")
+greet2('john',"400")
+greet2('john',23+40)
+
---
+++ 20211015/q1.py
@@ -0,0 +1,5 @@
+def greet():
+    print("MH")
+    print(1+2+3+4+5)
+
+greet()
