039c270f40171fbad782fa6e1e1611c3da912955
minggaaaa <92001539+minggaaaa@users.noreply.github.com>
2021-10-26 09:19:33 +0900

    java20211014

---
+++ 20211014/src/F.java
@@ -0,0 +1,14 @@
+
+public class F {
+	public static void main(String[] args) {
+		System.out.println("2 + 5 = " + 2 + 5);
+		System.out.println("2 + 5 = " + (2 + 5));
+		
+		
+		System.out.println(12);
+		System.out.println("12");
+		System.out.println("1"+2);
+		System.out.println(1 + "2");
+		System.out.println("1 + 2");
+	}
+}
