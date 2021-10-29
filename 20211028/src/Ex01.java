9a818df905fad5eef6ae917824f9fb711664b1a3
minggaaaa <92001539+minggaaaa@users.noreply.github.com>
2021-10-29 15:36:48 +0900

    java20211029

---
+++ 20211028/src/Ex01.java
@@ -0,0 +1,15 @@
+import aa.AA;
+
+public class Ex01 {
+	
+	public static void main(String[] args) {
+		AA a1 = new AA();
+		a1.doPrint();
+		
+		AA a2 = new AA(100);
+		a2.doPrint();
+		
+		AA a3 = new AA(200,"a3");
+		a3.doPrint();
+	}
+}
