bd3da01955405f29915c1b3bec6dc7a691cdd055
minggaaaa <92001539+minggaaaa@users.noreply.github.com>
2021-10-29 15:43:54 +0900

    java20211028

---
+++ 20211028/src/zoo/Cat.java
@@ -0,0 +1,18 @@
+package zoo;
+
+class Duck{}
+
+public class Cat {
+//	public void makeCat() {
+//		Duck duck = new Duck();
+//		System.out.println("makeDuck");
+//	}
+	public Cat() {}
+	public void a() {
+		System.out.println("cat.a");
+	}
+	
+	public void b() {
+		System.out.println("cat.b");
+	}
+}
